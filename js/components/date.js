c4dc4b784fd5332b3f9d4f3c8997019d1d680964
Tony Wu <twu@fliplet.com>
2020-06-16 15:08:26 +0100

    Start the week on Monday

--- js/components/date.js
+++ js/components/date.js
@@ -40,6 +40,7 @@ Fliplet.FormBuilder.field('date', {
       var $el = $(this.$el).find('input.date-picker').datepicker({
         format: "yyyy-mm-dd",
         todayHighlight: true,
+        weekStart: 1,
         autoclose: true
       }).on('changeDate', function(e) {
         var value = moment(e.date).format(DATE_FORMAT);
