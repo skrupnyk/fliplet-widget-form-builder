103d3100cac845aa231851eda62d5ae47799bbc9
Tony Wu <tonytlwu@users.noreply.github.com>
2021-06-30 16:25:30 +0100

    Ensures HTML element has a valid ID

--- js/components/wysiwyg.js
+++ js/components/wysiwyg.js
@@ -133,7 +133,7 @@ Fliplet.FormBuilder.field('wysiwyg', {
     var $vm = this;
     var lineHeight = 40;
 
-    this.tinymceId = this.name + parseInt($(this.$refs.textarea).parents('[data-form-builder-id]').data('formBuilderId'), 10);
+    this.tinymceId = _.kebabCase(this.name) + '-' + $(this.$refs.textarea).parents('[data-form-builder-id]').data('formBuilderId');
 
     var config = {
       target: this.$refs.textarea,
