646f05d4e7594f9687f342c0d67982bc364fd084
skrupnyk <serhii.kr@upplabs.com>
2021-11-09 11:11:09 +0200

    fix duplicate description fields

--- js/libs/core.js
+++ js/libs/core.js
@@ -364,7 +364,7 @@ Fliplet.FormBuilder = (function() {
 
       component.props._componentsWithDescription = {
         type: Array,
-        default: ['flInput', 'flCheckbox', 'flRadio', 'flEmail', 'flNumber', 'flTelephone', 'flUrl', 'flTextarea', 'flWysiwyg', 'flSelect', 'flDate', 'flTime', 'flStarRating', 'flSignature']
+        default: ['flInput', 'flCheckbox', 'flRadio', 'flEmail', 'flNumber', 'flTelephone', 'flUrl', 'flTextarea', 'flWysiwyg', 'flSelect', 'flDate', 'flTime']
       };
 
       component.props._readOnlyComponents = {
